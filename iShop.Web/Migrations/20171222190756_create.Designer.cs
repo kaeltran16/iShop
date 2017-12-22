e0e2743d45ee79eba6294e86d6b71d22b1c34cbb
hoideptrai123 <toilahoi007@gmail.com>
2017-12-23 02:11:51 +0700

    compile db

--- iShop.Web/Migrations/20171222190756_create.Designer.cs
+++ iShop.Web/Migrations/20171222190756_create.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace iShop.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171222190756_create")]
+    partial class create
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
