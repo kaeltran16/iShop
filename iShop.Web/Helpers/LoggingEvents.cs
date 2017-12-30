50a72d33d860820e25f3f123977aeeeddf46f4b6
Khang-Tran <piskael03@gmail.com>
2017-12-29 21:16:33 -0500

    Finish testing Account APIs

---
+++ iShop.Web/Helpers/LoggingEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iShop.Web.Helpers
+{
+    public class LoggingEvents
+    {
+        public const int Success = 1000;
+        public const int Fail = 2000;
+    }
+}
