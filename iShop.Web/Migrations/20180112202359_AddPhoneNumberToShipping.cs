473e85a42a9e4876b15cc477ac7787501efe7f0c
Khang-Tran <piskael03@gmail.com>
2018-01-12 15:25:34 -0500

    Add PhoneNumber to Shipping

---
+++ iShop.Web/Migrations/20180112202359_AddPhoneNumberToShipping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace iShop.Web.Migrations
+{
+    public partial class AddPhoneNumberToShipping : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Shippings",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Shippings");
+        }
+    }
+}
--- iShop.Web/Server/Core/Resources/ShippingResource.cs
+++ iShop.Web/Server/Core/Resources/ShippingResource.cs
@@ -24,6 +24,9 @@ namespace iShop.Web.Server.Core.Resources
         [Required]
         [StringLength(50)]
         public string City { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string PhoneNumber { get; set; }
 
 
         public ShippingResource()
